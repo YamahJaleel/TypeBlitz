9a0d9ae9751a61b85c8b2c7243f1ac8cbf455bbd
Yamah Jaleel <jaleel.y01@gmail.com>
2023-11-13 17:47:41 -0500

    Delete files

--- main.py
+++
@@ -1,9 +0,0 @@
-import type
-
-class Main:
-    def main(self):
-        print("Hello, this is the main function!")
-
-if __name__ == "__main__":
-    for _ in range(7):
-        type.generate_random_word()
